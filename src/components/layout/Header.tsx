eee9d6321b86f46194c3db4121912a8d2b06dfa5
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-06-24 15:47:57 +0900

    add: 헤더 컴포넌트 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,50 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import CommonButton from '../common/Button'
+
 export default function Header() {
+  const router = useRouter()
+
+  const handleLogout = () => {
+    if (confirm('정말 로그아웃하시겠습니까?')) {
+      router.push('/')
+    }
+  }
+
   return (
-    <div>
-      <h1>라인공영 Intra</h1>
-    </div>
-  );
+    <header className="top-0 left-0 w-full bg-primaryBlue border-b border-gray-200 shadow-sm z-50">
+      <div className="mx-auto flex items-center justify-between px-6 h-16">
+        <div className="flex items-center space-x-6">
+          <h1 className="text-2xl font-bold text-white">라인공영 Intra</h1>
+
+          <nav className="flex items-center space-x-4 text-sm text-gray-600">
+            <span
+              className="text-white border-b-2 transition"
+              // hover:text-black
+              // onClick={() => router.push('/')}
+            >
+              Home
+            </span>
+
+            <span className="text-black">&gt;&gt;</span>
+            <select className="bg-white border font-bold border-gray-300 rounded px-2 py-1 w-52 text-sm focus:outline-none focus:ring-1 focus:ring-blue-400">
+              <option>사업장 관리</option>
+              <option>외주 관리</option>
+            </select>
+            <span className="text-black">&gt;&gt;</span>
+            <select className="bg-white border font-bold border-gray-300 rounded px-2 py-1 w-52 text-sm focus:outline-none focus:ring-1 focus:ring-blue-400">
+              <option>등록</option>
+              <option>수정</option>
+            </select>
+          </nav>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <p className="text-sm text-white">이경호(로그인한 ID)</p>
+          <CommonButton label="로그아웃" variant="secondary" onClick={handleLogout} className="text-sm" />
+        </div>
+      </div>
+    </header>
+  )
 }
