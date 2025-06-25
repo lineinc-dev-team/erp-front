a318ef7d201fe899d9768874071b7854d9874dec
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-06-25 13:24:59 +0900

    feat: 사업장 관리 공통 레이아웃 추가

--- src/app/(views)/business/layout.tsx
+++ src/app/(views)/business/layout.tsx
@@ -1,9 +1,9 @@
 // app/business/layout.tsx
 export default function BusinessLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <nav className="bg-gray-100 p-4">비즈니스 전용 메뉴</nav>
-      <main>{children}</main>
+    <div className="m-8">
+      <h1 className="text-2xl mb-3">사업장 관리</h1>
+      {children}
     </div>
   )
 }
