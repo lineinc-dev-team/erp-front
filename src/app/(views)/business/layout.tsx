72b5b6613cd0d5826b228d500235a7cba626b3db
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-06-26 15:15:26 +0900

    add : 사업장 등록과 관리 제목 부분 해당 url에 따라 동적이게 변경

--- src/app/(views)/business/layout.tsx
+++ src/app/(views)/business/layout.tsx
@@ -1,8 +1,14 @@
-// app/business/layout.tsx
+'use client'
+
+import { useSelectedLayoutSegment } from 'next/navigation'
+
 export default function BusinessLayout({ children }: { children: React.ReactNode }) {
+  const segment = useSelectedLayoutSegment()
+  const title = segment === 'registration' ? '사업장 등록' : '사업장 관리'
+
   return (
     <div className="m-8">
-      <h1 className="text-2xl mb-3">사업장 관리</h1>
+      <h1 className="text-2xl mb-3">{title}</h1>
       {children}
     </div>
   )
