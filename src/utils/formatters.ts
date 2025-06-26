deb7e94632e69c72152a568474a2a12e4b5f9275
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-06-26 15:23:59 +0900

    add: 오늘 날짜 상수 파일로 분리

---
+++ src/utils/formatters.ts
@@ -0,0 +1,9 @@
+export const getTodayDateString = (): string => {
+  const today = new Date()
+
+  const year = today.getFullYear()
+  const month = String(today.getMonth() + 1).padStart(2, '0') // 두 자리 수
+  const day = String(today.getDate()).padStart(2, '0') // 두 자리 수
+
+  return `${year}-${month}-${day}`
+}
