fd791573afc6cc1a5221d5d825831af7f2eb57fa
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-06-30 15:30:19 +0900

    add: 개인정보 api 추가

--- src/api/config/env.ts
+++ src/api/config/env.ts
@@ -8,5 +8,6 @@ export const API = {
   BASE_URL: apiBaseUrl,
   LOGIN: `${apiBaseUrl}/auth/login`,
   LOGOUT: `${apiBaseUrl}/auth/logout`,
+  AUTH: `${apiBaseUrl}/auth/me`,
   // 다른 API endpoint도 여기에 추가 가능
 }
