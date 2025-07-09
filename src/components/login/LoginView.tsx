661bb8e7983c77ee84b1b1617f00a379949b5669
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-07-09 17:28:37 +0900

    fix: yml 코드 수정(4)

--- src/components/login/LoginView.tsx
+++ src/components/login/LoginView.tsx
@@ -21,7 +21,7 @@ export default function LoginView() {
 
   return (
     <div>
-      <h1 className="text-[42px] mb-4">라인공영 관리 시스템 Test</h1>
+      <h1 className="text-[42px] mb-4">라인공영 관리 시스템 Test~</h1>
 
       <div className="w-88 flex flex-col gap-2 m-0">
         <CommonInput
