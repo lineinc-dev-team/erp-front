d11f184318501ef228417cb5c72fc5ca3c815e3c
kyoungho <kyoungho@kyounghoui-MacBookAir.local>
2025-12-02 13:18:05 +0900

    Fix deployment pipeline(15)

--- src/components/login/LoginView.tsx
+++ src/components/login/LoginView.tsx
@@ -40,7 +40,7 @@ export default function LoginView() {
         className="mb-4 "
       />
 
-      <h1 className="text-[42px] mb-4">라인공영 관리 시스템 </h1>
+      <h1 className="text-[42px] mb-4">라인공영 관리 시스템(운영) </h1>
 
       <div className="w-88 flex flex-col gap-2 m-0">
         <CommonInput
